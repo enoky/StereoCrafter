7ff6d10805396b9947100bdc54f6f1658c426484
irgadget <114357496+Billynom8@users.noreply.github.com>
2026-02-21 11:18:28 +0000

    update splatting version number

--- splatting_gui.py
+++ splatting_gui.py
@@ -87,7 +87,7 @@ except:
     logger.info("Forward Warp Pytorch is active.")
 from dependency.video_previewer import VideoPreviewer
 
-GUI_VERSION = "26-02-07.1"
+GUI_VERSION = "26-02-21.0"
 
 
 # [REFACTORED] FusionSidecarGenerator class replaced with core import
